71db15e1252b3cfdeeec0734b115ed3b3a1d2f12
Arnaud Morin <arnaud.morin@gmail.com>
2020-01-22 20:02:13 +0100

    Up
    Signed-off-by: Arnaud Morin <arnaud.morin@gmail.com>

--- extra/reader.sh
+++ extra/reader.sh
@@ -5,10 +5,14 @@ while true ; do
     data=$(curl -s -q http://127.0.0.1:5000/read)
     FROM=$(echo $data | jq '.from' | sed -r 's/\+/%2B/; s/"//g')
     MESSAGE=$(echo $data | jq '.message' | sed -r 's/"//g')
+    DATE=$(date -R)
     #echo "DEBUG $FROM / $MESSAGE"
     if [ "Z$FROM" != "Znull" -a "Z$MESSAGE" != "Znull" ] ; then
-        echo "Message received: $FROM / $MESSAGE"
-        echo "Message received: $FROM / $MESSAGE" >> /var/log/sms.log
+        echo "$DATE | $FROM | $MESSAGE"
+        echo "$DATE | $FROM | $MESSAGE" >> /var/log/sms.log
+        if [ "$MESSAGE" = "ping" ] ; then
+            ~/.local/bin/sendsms.sh 0${FROM#%2B33} pong
+        fi
     fi
     sleep 1
 done
